e60933ac3ae6d882b2fc895fc35fe4479238846c
jiajia Liang <jl9pg@virginia.edu>
2020-11-17 11:54:03 -0500

    add testing oracle

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class NeuralModel:
     def run_example(self, sentence):
         inputs = self.tokenizer(sentence, return_tensors="pt")
         with torch.no_grad():
-            # ((positive_logit, negative_logit), ([bs, sent_len, dim]x7))
+            # ((negative_logit, positive_logit), ([bs, sent_len, dim]x7))
             # Outputs is a tuple has two items, sentiment predictions and neural network intermediate values
             # Those values consist of seven tensors (matrix), from layer 0 (can be ignored) to layer 6 (last layer, important!)
             # The size of each matrix is num_of_sentences (1) by maximum_sentence_length by number_of_neuron (768)
@@ -40,7 +40,7 @@ class NeuralModel:
             # Return only last layer states of the [CLS] token for now.
             output_state = outputs[1][-1][0, 0].cpu().numpy()
         # This converts the prediction (logits) to probability (sum to 1)
-        # prob_positive, prob_negative
+        # prob_negative,prob_positive
         scores = np.exp(predictions) / np.exp(predictions).sum(-1, keepdims=True)
         return scores, output_state
 
@@ -83,12 +83,45 @@ def Perturb_add_negation_phrase(dataset):
         return ['%s %s' % (x, p) for p in phrases]
     return Perturb.perturb(dataset, add_negative_phrase)
 
-def runPerburtation(perturb):
-    for testPair in perturb.data:
-        for test in testPair: #metamorphic test pair 
-            pred, output_state = model.run_example(test)
-            print(pred, test)
+def run_perburtation(perturb, expect_bahavior):
+    for test_pair in perturb.data:
+        print ("Pass(1) or fail(0): ", oracle_test(test_pair,expect_bahavior)) #0 if metamorphic test fail, 1 if success
 
+
+def oracle_test(test_pair, expect_bahavior):#return 0 if metamorphic test fail, 1 if success
+    sentiment = []
+    print (test_pair)
+    for sentence in test_pair:
+        pred, output_state = model.run_example(sentence)
+        '''
+        Need to add threshold?
+        '''
+        if pred[0][0] > pred[0][1]:
+            sentiment.append(-1) #negative 
+        elif pred[0][0] < pred[0][1]:
+            sentiment.append(1) #positive
+        else:   
+            sentiment.append(0) #neutral 
+
+    if expect_bahavior == "INV": #prediction should not change
+        print ("sentiment prediction",sentiment)    
+        success = all(ele == sentiment[0] for ele in sentiment) 
+        return 1 if success else 0
+
+    elif expect_bahavior == "CHANGE": #prediction should change
+        print ("sentiment prediction",sentiment)   
+        if sentiment[0] != sentiment[1]:
+            return 1 #pass if original and negation sentence prediction change
+        return 0 #fail
+
+    elif expect_bahavior == "MONO_DEC": #prediction should change
+        #p7 sentiment = sentiment of new,old pair: [new, old, new, old]
+        #sentiment at even index i should not be less than i+1 
+        print ("sentiment prediction",sentiment)   
+        for i in range(0, len(sentiment), 2): 
+            if sentiment[i] < sentiment[i+1]:
+                return 0 #fail
+        return 1 #pass if all pairs' is mono decreasing
 
 if __name__ == "__main__":
     test_sent = "We are very happy to include pipeline into the transformers repository."
@@ -122,7 +155,7 @@ if __name__ == "__main__":
             'Mary Keen was brilliant!', 
             'I hated everything about New York.',
             'This movie was very bad!',
-            'Jerry gave me 8 delicious apples',
+            'Jerry gave me 8 delicious apples.',
             'I really liked this movie.',
             'just bad.',
             'amazing.',
@@ -132,42 +165,42 @@ if __name__ == "__main__":
     pdataset = list(nlp.pipe(dataset))
 
     editor = Editor()
-    # Perturbation1: add typo eg. amazing --> mmazing
+    # Perturbation1-invariant: add typo eg. amazing --> mmazing
     p1 = Perturb_add_typo(dataset)
     
-    #Perturbation2: change name 
+    #Perturbation2-invariant: change name 
     p2 = Perturb_change_names(pdataset)
 
-    #Perturbation3: change location eg. New York --> CVille
+    #Perturbation3-invariant: change location eg. New York --> CVille
     p3 = Perturb_change_location(pdataset)
 
-    #Perturbation4: change numbers eg. 1 --> 3
+    #Perturbation4-invariant: change numbers eg. 1 --> 3
     p4 = Perturb_change_number(pdataset)
 
-    #Perturbation5: add or remove punctuation. (might not be very useful though)
+    #Perturbation5-invariant: add or remove punctuation. (might not be very useful though)
     p5 = Perturb_punctuation(pdataset)
 
     #Perturbation6: add negation eg. like --> don't like 
     #negation function doesn't work on many sentences, create a seperate dataset that is application. 
     # Might drop this perturbation and use Perturbation7 instead. 
-    dataset_negation = ['Mary Keen was brilliant.', 'This movie was bad.', "Jerry is amazing", "You are my friend"] 
+    dataset_negation = ['Mary Keen was brilliant.', 'This movie was bad.', "Jerry is amazing.", "You are my friend."] 
     pdataset_negation = list(nlp.pipe(dataset_negation))
     p6 = Perturb_add_negation(pdataset_negation)
 
     #Perturbation7: add negation phrase at the end of the sentence
     p7 = Perturb_add_negation_phrase(dataset)
-    
+
     print ("---------------metamorphic relation 1: add typo ------------------")
-    runPerburtation(p1)
+    run_perburtation(p1,"INV")
     print ("---------------metamorphic relation 2: change name ------------------")
-    runPerburtation(p2)
+    run_perburtation(p2,"INV")
     print ("---------------metamorphic relation 3: change location ------------------")
-    runPerburtation(p3)
+    run_perburtation(p3,"INV")
     print ("---------------metamorphic relation 4: change numbers ------------------")
-    runPerburtation(p4)
+    run_perburtation(p4,"INV")
     print ("---------------metamorphic relation 5: add/remove punc ------------------")
-    runPerburtation(p5)
+    run_perburtation(p5,"INV")
     print ("---------------metamorphic relation 6: add negation ------------------")
-    runPerburtation(p6)
+    run_perburtation(p6,"CHANGE")
     print ("---------------metamorphic relation 7: add negation phrase------------------")
-    runPerburtation(p7)
+    run_perburtation(p7,"MONO_DEC")
